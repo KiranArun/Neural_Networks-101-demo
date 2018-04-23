9bf9aa16df667a51e2aeea0cb5cbe0ad838ee4fb
KiranArun <kironni@gmail.com>
2018-04-23 16:53:46 +0100

    added copy helper funcs to notebook directory to setup

--- scripts/setup.py
+++ scripts/setup.py
@@ -2,7 +2,9 @@ import subprocess
 import sys
 import os
 
-if sys.argv[1] == 'pytorch':
-	subprocess.call("./scripts/install_pytorch.py")
-elif sys.argv[1] == 'tensorboard':
-	subprocess.call("./scripts/run_tensorboard.sh")
+if 'pytorch' in sys.argv[1:]:
+	subprocess.call("/content/Neural_Networks-demo/scripts/install_pytorch.py")
+if 'tensorboard' in sys.argv[1:]:
+	subprocess.call("/content/Neural_Networks-demo/scripts/run_tensorboard.sh")
+if 'helper_funcs' in sys.argv[1:]:
+	subprocess.call("cp /content/Neural_Networks-demo/helper_funcs.py /content/")
