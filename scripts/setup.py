95f85a2a512383f99ef955792ea6adef5b38dbae
KiranArun <kironni@gmail.com>
2018-04-23 16:57:47 +0100

    fixed lack of execution commands in setup

--- scripts/setup.py
+++ scripts/setup.py
@@ -3,8 +3,8 @@ import sys
 import os
 
 if 'pytorch' in sys.argv[1:]:
-	subprocess.call("/content/Neural_Networks-demo/scripts/install_pytorch.py")
+	subprocess.call("python3 /content/Neural_Networks-demo/scripts/install_pytorch.py")
 if 'tensorboard' in sys.argv[1:]:
-	subprocess.call("/content/Neural_Networks-demo/scripts/run_tensorboard.sh")
+	subprocess.call("./content/Neural_Networks-demo/scripts/run_tensorboard.sh")
 if 'helper_funcs' in sys.argv[1:]:
 	subprocess.call("cp /content/Neural_Networks-demo/helper_funcs.py /content/")
