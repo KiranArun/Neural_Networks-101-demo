b330b6ca1f8b1b62a9393c2f80fda9e05bed713b
KiranArun <kironni@gmail.com>
2018-04-23 17:47:50 +0100

    fixing bugs in tensorboard and pytorch setup scripts

--- scripts/install_pytorch.py
+++ scripts/install_pytorch.py
@@ -5,4 +5,5 @@ import subprocess
 from wheel.pep425tags import get_abbr_impl, get_impl_ver, get_abi_tag
 platform = '{}{}-{}'.format(get_abbr_impl(), get_impl_ver(), get_abi_tag())
 accelerator = 'cu80' if path.exists('/opt/bin/nvidia-smi') else 'cpu'
-subprocess.call('pip install -q http://download.pytorch.org/whl/{}/torch-0.3.0.post4-{}-linux_x86_64.whl torchvision'.format(accelerator,platform))
+
+subprocess.call('pip3 install -q'.split() + ['http://download.pytorch.org/whl/%s/torch-0.3.0.post4-%s-linux_x86_64.whl torchvision' % (accelerator,platform)])
--- scripts/run_tensorboard.sh
+++ scripts/run_tensorboard.sh
@@ -2,13 +2,11 @@
 
 echo "Running tensorboard..."
 
-tensorboard --logdir=/content/Neural_Networks-demo/logdir/ --host=0.0.0.0 --port=6006 & true;
+tensorboard --logdir=/content/Neural_Networks-demo/logdir/ --host=0.0.0.0 --port=6006 &
 
-if ! [ -e /content/ngrok ]
-then
-    wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip > /dev/null 2>&1
-    unzip -o ngrok-stable-linux-amd64.zip > /dev/null 2>&1
-fi
+wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip > /dev/null 2>&1
+unzip -o ngrok-stable-linux-amd64.zip > /dev/null 2>&1
 
-./ngrok http 6006 & true;
+/content/ngrok http 6006 &
+
 curl -s http://localhost:4040/api/tunnels | python3 -c "import sys, json; print(json.load(sys.stdin)['tunnels'][0]['public_url'])"
