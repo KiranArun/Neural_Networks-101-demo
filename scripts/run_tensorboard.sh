e296bbdce92b0820c78265d6fd7f4901ecccca52
KiranArun <kironni@gmail.com>
2018-04-24 19:23:27 +0100

    removed running tensorboard and ngrok from scripts as it doesn't work with colab

--- scripts/run_tensorboard.sh
+++ scripts/run_tensorboard.sh
@@ -2,14 +2,8 @@
 
 echo "Running tensorboard..."
 
-python3 -c "import subprocess; subprocess.Popen('tensorboard --logdir=/content/Neural_Networks-demo/logdir/ --host=0.0.0.0 --port=6006'.split())"
-
 if ! [ -e /content/ngrok ]
 then
     wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip > /dev/null 2>&1
     unzip -o ngrok-stable-linux-amd64.zip > /dev/null 2>&1
 fi
-
-python3 -c "import subprocess; subprocess.Popen('/content/ngrok http 6006'.split())"
-
-curl -s http://localhost:4040/api/tunnels | python3 -c "import sys, json; print('Tensorboard Link:', json.load(sys.stdin)['tunnels'][0]['public_url'])"
