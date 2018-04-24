f9fde0ae9f04663f5529062eded5b56857fe0dcd
KiranArun <kironni@gmail.com>
2018-04-24 16:59:54 +0100

    fixing bugs

--- scripts/run_tensorboard.sh
+++ scripts/run_tensorboard.sh
@@ -2,14 +2,12 @@
 
 echo "Running tensorboard..."
 
-python3 /content/Neural_Networks-demo/scripts/run_tensorboard_command.py
-
 if ! [ -e /content/ngrok ]
 then
     wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip > /dev/null 2>&1
     unzip -o ngrok-stable-linux-amd64.zip > /dev/null 2>&1
 fi
 
-/content/ngrok http 6006 &
+/content/ngrok http 6006
 
 curl -s http://localhost:4040/api/tunnels | python3 -c "import sys, json; print('Tensorboard Link:', json.load(sys.stdin)['tunnels'][0]['public_url'])"
--- scripts/run_tensorboard_command.py
+++
@@ -1,3 +0,0 @@
-import os
-
-os.system("tensorboard --logdir=/content/Neural_Networks-demo/logdir/ --host=0.0.0.0 --port=6006 &")
