726a63a55d5b17e10940b5835026c5ee32d3e858
KiranArun <kironni@gmail.com>
2018-04-25 09:29:26 +0100

    changed path to files due to repo name change

--- scripts/setup.py
+++ scripts/setup.py
@@ -3,13 +3,13 @@ import sys
 import os
 
 if 'pytorch' in sys.argv[1:]:
-	subprocess.call(['python3', '/content/Neural_Networks-demo/scripts/install_pytorch.py'])
+	subprocess.call(['python3', '/content/Neural_Networks-101-demo/scripts/install_pytorch.py'])
 
 if 'tensorboard' in sys.argv[1:]:
-	subprocess.call(['chmod', '+x', '/content/Neural_Networks-demo/scripts/run_tensorboard.sh'])
-	subprocess.call(['/content/Neural_Networks-demo/scripts/run_tensorboard.sh'])
+	subprocess.call(['chmod', '+x', '/content/Neural_Networks-101-demo/scripts/run_tensorboard.sh'])
+	subprocess.call(['/content/Neural_Networks-101-demo/scripts/run_tensorboard.sh'])
 	
 
 if 'helper_funcs' in sys.argv[1:]:
 	print('Getting helper functions...')
-	subprocess.call(['cp', '/content/Neural_Networks-demo/helper_funcs.py', '/content/'])
+	subprocess.call(['cp', '/content/Neural_Networks-101-demo/helper_funcs.py', '/content/'])
