6731771d9dbb498da1e9da34957e646ae4a2ddc4
KiranArun <kironni@gmail.com>
2018-05-02 21:42:10 +0100

    renamed run_tensorboard to install_ngrok

--- scripts/setup.py
+++ scripts/setup.py
@@ -6,8 +6,8 @@ if 'pytorch' in sys.argv[1:]:
 	subprocess.call(['python3', '/content/Neural_Networks-101-demo/scripts/install_pytorch.py'])
 
 if 'tensorboard' in sys.argv[1:]:
-	subprocess.call(['chmod', '+x', '/content/Neural_Networks-101-demo/scripts/run_tensorboard.sh'])
-	subprocess.call(['/content/Neural_Networks-101-demo/scripts/run_tensorboard.sh'])
+	subprocess.call(['chmod', '+x', '/content/Neural_Networks-101-demo/scripts/install_ngrok.sh'])
+	subprocess.call(['/content/Neural_Networks-101-demo/scripts/install_ngrok.sh'])
 	
 
 if 'helper_funcs' in sys.argv[1:]:
